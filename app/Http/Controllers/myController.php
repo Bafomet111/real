cbf37943883a91f9e8670e797492a383694fb0e0
Ilya <ilya@gmail.com>
2020-01-13 08:50:37 +0300

    hello

--- app/Http/Controllers/myController.php
+++ app/Http/Controllers/myController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class myController extends Controller
 {
+    private $a;
+
+    public function __construct(User $a)
+    {
+        $this->a = $a;
+    }
+
     public function show($name = 'admin')
     {
         return view('my_view', ['name' => $name]);
